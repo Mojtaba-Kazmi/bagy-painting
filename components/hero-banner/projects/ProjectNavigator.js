222d984cecec38daf53f9e72040bc20fd8dfbd5a
mojtaba-kazmi <kazmi.saidmojtaba@gmail.com>
2025-02-06 10:21:39 +0100

    project responsive updated

--- components/hero-banner/projects/ProjectNavigator.js
+++ components/hero-banner/projects/ProjectNavigator.js
@@ -7,28 +7,28 @@ import { CldImage } from "next-cloudinary";
 const projects = [
   {
     id: 1,
-    title: "Quality Painting Services - Adelaide, SA",
+    title: "Our Latest Work",
     description:
       "Successfully completed painting project in Adelaide, SA, delivering a smooth and durable finish with attention to detail.",
     image: "recent_h9qjq3",
   },
   {
     id: 2,
-    title: "Quality Painting Services - Adelaide, SA",
+    title: "Our Latest Work",
     description:
       "Our team provided professional painting services, ensuring a high-quality result that enhances the overall appearance.",
     image: "recent_pro_xelbm0",
   },
   {
     id: 3,
-    title: "Quality Painting Services - Adelaide, SA",
+    title: "Our Latest Work",
     description:
       "Bagy Painting completed another successful project in Adelaide, SA, focusing on precision, durability, and a flawless finish.",
     image: "recent_project_bagy_painting_v0o6de",
   },
   {
     id: 4,
-    title: "Quality Painting Services - Adelaide, SA",
+    title: "Our Latest Work",
     description:
       "Expert painting services delivered with a commitment to excellence, bringing long-lasting results to our clients in Adelaide, SA.",
     image: "recent_projects_c8n0df",
@@ -77,8 +77,8 @@ export default function ProjectNavigator() {
         <CldImage
           key={currentIndex} // Forces re-render on change
           src={image}
-          width={600}
-          height={600}
+          width={300}
+          height={300}
           alt={title}
           className={`${styles.projectImage} ${!isFading ? styles.active : ""}`}
         />
